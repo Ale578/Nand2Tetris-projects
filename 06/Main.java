6de4c952d67f7117c104d1da9e19c9ecd2d7d201
Ale579 <alessandrotg9@gmail.com>
2025-09-04 10:07:23 +0200

    Clean Main code and make it so asm files are accessed by command line argument

--- 06/Main.java
+++ 06/Main.java
@@ -10,67 +10,68 @@ import java.util.List;
  */
 public class Main {
     public static void main(String[] args) {
-        String program = "assemblyPrograms/Max.asm";
+        String program = args[0];
         String binaryCommand;
         Path path = Paths.get(program.substring(17, program.indexOf(".")) + ".hack");
         List<String> binaryCommands = new ArrayList<>();
         SymbolTable symbolTable = new SymbolTable();
+        Parser parser;
 
-        // First pass: Populate symbol table with labels
         try {
-            Parser symbolParser = new Parser(program);
+            // First pass: Populate symbol table with labels
+            parser = new Parser(program);
             int commandAddress = 0;
 
-            while (symbolParser.hasMoreLines()) {
-                symbolParser.advance();
+            while (parser.hasMoreLines()) {
+                parser.advance();
 
-                if (symbolParser.commandType().equals("L_COMMAND") && !symbolTable.contains(symbolParser.symbol())) {
-                    symbolTable.addEntry(symbolParser.symbol(), commandAddress);
-                } else if (!symbolParser.commandType().equals("NO_COMMAND")) {
+                boolean isL_Command = parser.commandType().equals("L_COMMAND");
+                boolean inSymbolTable = !symbolTable.contains(parser.symbol());
+                boolean isAnyCommand = !parser.commandType().equals("NO_COMMAND");
+
+                if (isL_Command && inSymbolTable) {
+                    symbolTable.addEntry(parser.symbol(), commandAddress);
+                } else if (isAnyCommand) {
                     commandAddress++;
                 }
             }
-            symbolParser.close();
-        } catch (IOException e) {
-            System.err.println("Unable to open " + program);
-        }
 
-        // Second pass: Translate code from assembly to machine code and write output file
-        try {
-            Parser translationParser = new Parser(program);
+            // Second pass: Translate code from assembly to machine code and write output file
+            parser = new Parser(program);
             int availableAddress = 16;
-            while (translationParser.hasMoreLines()) {
-                translationParser.advance();
+            while (parser.hasMoreLines()) {
+                parser.advance();
 
-                if (translationParser.commandType().equals("C_COMMAND")) {
-                    binaryCommand = "111" + Code.comp(translationParser.comp()) + Code.dest(translationParser.dest()) + Code.jump(translationParser.jump());
+                boolean isC_Command = parser.commandType().equals("C_COMMAND");
+                boolean isA_Command = parser.commandType().equals("A_COMMAND");
+
+                if (isC_Command) {
+                    binaryCommand = "111" + Code.comp(parser.comp()) + Code.dest(parser.dest()) + Code.jump(parser.jump());
                     binaryCommands.add(binaryCommand); 
 
-                } else if (translationParser.commandType().equals("A_COMMAND")) {
-                    String symbol = translationParser.symbol();
+                } else if (isA_Command) {
+                    String symbol = parser.symbol();
+                    boolean isA_CommandWithLabel = symbol.matches("[a-zA-Z_.$][a-zA-Z0-9_.$]*");
 
-                    // Handle labels
-                    if (symbol.matches("[a-zA-Z_.$][a-zA-Z0-9_.$]*") && !symbol.matches("\\d+")) {
-                        // If label is in symbol table, retrieve the corresponding address
+                    if (isA_CommandWithLabel) {
                         if (symbolTable.contains(symbol)) {
                             symbol = Integer.toString(symbolTable.GetAddress(symbol));
-                        // Add new variables with an available address to symbol table
+
                         } else {
                             symbolTable.addEntry(symbol, availableAddress);
                             symbol = Integer.toString(availableAddress);
                             availableAddress++;
                         }
                     }
-
                     binaryCommand = Code.number(Integer.parseInt(symbol));
                     binaryCommands.add(binaryCommand); 
                 }
             }
-            translationParser.close();
-
+            parser.close();
             Files.write(path, binaryCommands);
         } catch (IOException e) {
             System.err.println("Unable to open " + program);
         }
+
     }
 }
