a57a72692b57f6777adb7ef457d08d0b761b3bd6
Ale579 <alessandrotg9@gmail.com>
2025-12-23 10:13:41 +0100

    Make VM translator capable of stack arithmetic

--- 07/CodeWriter.java
+++ 07/CodeWriter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 public class CodeWriter {
     BufferedWriter writer;
+    static int labelCount = 0;
 
     CodeWriter(String outputFile) {
         try {
@@ -17,21 +18,65 @@ public class CodeWriter {
     // }
 
     public void writeArithmetic(String command) throws IOException {
-        writer.write("// " + command + "\n");
+        //writer.write("// " + command + "\n");
+        // Access top value
         writer.write("@SP\n");
         writer.write("M=M-1\n");
         writer.write("A=M\n");
-        if (command.equals("add")) {
-            writer.write("D=M\n");
-            writer.write("@SP\n");
-            writer.write("M=M-1\n");
-            writer.write("A=M\n");
-            writer.write("M=D+M\n");
+        writer.write("D=M\n");
+
+        if (command.equals("neg")) {
+            writer.write("M=-D\n");
             writer.write("@SP\n");
             writer.write("M=M+1\n");
-        } else if (command.equals("sub")) {
-
+            return;
+        } else if (command.equals("not")) {
+            writer.write("M=!D\n");
+            writer.write("@SP\n");
+            writer.write("M=M+1\n");
+            return;
         }
+        // Access next top value
+        writer.write("@SP\n");
+        writer.write("M=M-1\n");
+        writer.write("A=M\n");
+        
+        if (command.equals("add")) {
+            writer.write("M=D+M\n");
+
+        } else if (command.equals("sub")) {
+            writer.write("M=M-D\n");
+
+        } else if (command.equals("eq") || command.equals("lt") || command.equals("gt")) {
+            String jumpCommand = "D;J" + command.toUpperCase() + "\n";
+            writer.write(
+                "D=M-D\n" +
+                "@TRUE_" + labelCount + "\n" +
+                jumpCommand +
+                "\n" +
+                "// Not true\n" +
+                "@SP\n" +
+                "A=M\n" +
+                "M=0\n" +
+                "@SKIP" + labelCount + "\n" +
+                "0;JMP\n" +
+                "\n" +
+                "(TRUE_" + labelCount + ")\n" +
+                "@SP\n" +
+                "A=M\n" +
+                "M=-1\n" +
+                "(SKIP" + labelCount + ")\n"
+            );
+            labelCount++;
+        } else if (command.equals("and")) {
+            writer.write("M=D&M\n");
+
+        } else if (command.equals("or")) {
+            writer.write("M=D|M\n");
+        }
+
+        writer.write("@SP\n");
+        writer.write("M=M+1\n");
     }
 
     public void writePushPop(String command, String segment, int index) throws IOException {
